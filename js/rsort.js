878d6e9966620c1b1b9f1ba63686f8284c345f83
Mohammed Salih k <salihsha656@gmail.com>
2025-09-04 14:26:58 +0530

    rsort

---
+++ js/rsort.js
@@ -0,0 +1,11 @@
+const arr=[5,4,2,5,3,1];
+for (let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[i]<arr[j]){
+            let t=arr[i];
+            arr[i]=arr[j];
+            arr[j]=t;
+        }
+    }
+}
+console.log(arr)
